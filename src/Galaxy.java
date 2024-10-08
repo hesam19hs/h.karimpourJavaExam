9d2ce6f29a04e4f261300baf416baec9962a3759
Hesam <hesam19hs@gmaile.com>
2024-10-08 14:15:06 +0330

    4

---
+++ src/Galaxy.java
@@ -0,0 +1,7 @@
+import java.util.List;
+
+public class Galaxy {
+    private String name;
+    private List<Planets> planets;
+
+}
---
+++ src/Planets.java
@@ -0,0 +1,6 @@
+public class Planets {
+    private String name;
+    private PlanetsType planetsType;
+    private String numberOfMoons;
+    private String distanceFromSun;
+}
