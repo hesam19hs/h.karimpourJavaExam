8e76189e2659f25b68797afeb6c5d442410c0dce
Hesam <hesam19hs@gmaile.com>
2024-10-08 15:28:24 +0330

    6

--- src/Galaxy.java
+++ src/Galaxy.java
@@ -4,4 +4,8 @@ public class Galaxy {
     private String galaxyName;
     private List<Planets> planets;
 
+    public Galaxy(String galaxyName) {
+        this.galaxyName = galaxyName;
+        this.planets = planets;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hi N!");
+        Scanner scanner = new Scanner(System.in);
+        Galaxy galaxy = defineGalaxy(scanner);
+
+    }
+
+    private static Galaxy defineGalaxy(Scanner scanner) {
+        while (true){
+            try{
+                System.out.println("Enter Galaxy name: ");
+                String glxyName = scanner.nextLine();
+                if(glxyName.trim().equals("")){
+                    throw new Exception("Invalid name");
+                }
+                Galaxy galaxy = new Galaxy(glxyName);
+                return galaxy;
+            } catch (Exception e) {
+                System.err.println(e.getMessage());
+            }
+        }
     }
 }
