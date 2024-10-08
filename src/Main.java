ea2d1735299bbfb0b3ecf5f28b0407d45bbf146d
Hesam <hesam19hs@gmaile.com>
2024-10-08 13:28:55 +0330

    3

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hi Nima!");
-        System.out.println("Hi Hesam!");
+        System.out.println("Hi N!");
     }
 }
